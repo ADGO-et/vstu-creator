156ff71470c12539ef19bb0071afce923202c54b
Melakeselam Yitbarek <111082914+melegithubyit@users.noreply.github.com>
2025-04-14 00:37:14 +0300

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "https://vstu.et:4001/api";
+// const BASE_URL = "https://vstu.et:4001/api";
 // const BASE_URL = "https://vstu.et:5059/api";     
-// const BASE_URL = "https://teletemari-backend-test-three.onrender.com";
+const BASE_URL = "https://api.vstu.et:4003/api";
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
